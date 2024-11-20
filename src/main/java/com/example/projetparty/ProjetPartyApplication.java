3d3002eb1d2aaa85db129fd551dda81841667c28
soumaya <soumayabenhamadi19@gmail.com>
2024-11-20 11:26:54 +0100

    first commit

---
+++ src/main/java/com/example/projetparty/ProjetPartyApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetPartyApplication.class, args);
+    }
+
+}
